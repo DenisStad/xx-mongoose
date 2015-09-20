8e6f08e508d3f594f7d682538fc99772a579e32c
DenisStad <denis@Deniss-MBP.home>
2015-09-20 21:50:18 +0100

    Add support for private fields and instance methods

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Define your models, for example
 App.models.project = {
   mongooseSchema: {
     name: { type: String },
-    date: { type: Date, default: Date.now }
+    date: { type: Date, default: Date.now, private: true }
   },
   //this will be called before registering the Schema
   mongoosePremodel: function(Schema) {
@@ -18,9 +18,17 @@ App.models.project = {
       next();
     });
   }
+  instanceMethod: {
+    doSomething: function() {}
+  }
 };
 ```
 
 When you're done defining them, add `App.load('mongoose/model');` to your app.js.
 If you pass an additional string, for example `App.load('mongoose/model', 'mongodb://localhost/my_database);`, mongoose will connect to that database.
 Otherwise it will connect to the database defined in `App.configuration.mongoose.url`
+
+Every method from `instanceMethods` will be added as instance methods to the schema.
+
+In addition to mongoose, you can define `private` on attributes. This will hide these attributes when callig `toJSON`.
+Useful for sensitive information
--- model.js
+++ model.js
@@ -7,7 +7,7 @@
  * App.models.project = {
  *   mongooseSchema: {
  *     name: { type: String },
- *     date: { type: Date, default: Date.now }
+ *     date: { type: Date, default: Date.now, private: true }
  *   },
  *   mongoosePremodel: function(Schema) {
  *     Schema.path('name').set(function(v) {
@@ -17,6 +17,9 @@
  *       // do something
  *       next();
  *     });
+ *   },
+ *   instanceMethod: {
+ *    doSomething: function() {}
  *   }
  * };
  * ```
@@ -24,10 +27,16 @@
  * When you're done defining them, add `App.load('mongoose/model');` to your app.js.
  * If you pass an additional string, for example `App.load('mongoose/model', 'mongodb://localhost/my_database);`, mongoose will connect to that database.
  * Otherwise it will connect to the database defined in `App.configuration.mongoose.url`
+ *
+ * Every method from `instanceMethods` will be added as instance methods to the schema.
+ *
+ * In addition to mongoose, you can define `private` on attributes. This will hide these attributes when callig `toJSON`.
+ * Useful for sensitive information
  * 
  */
 
 var mongoose = require('mongoose');
+var jsonSelect = require('mongoose-json-select');
 
 exports = module.exports = function(App, connectURL) {
   
@@ -39,6 +48,17 @@ exports = module.exports = function(App, connectURL) {
   for (var name in App.models) {
     if (App.models[name].mongooseSchema) {
       var Schema = new Schema(App.models[name].mongooseSchema);
+
+      for (var a in App.models[name].mongooseSchema) {
+        if (App.models[name].moongoseSchema[a].private) {
+          Schema.plugin(jsonSelect, '-' + a);
+        }
+        Schema.methods[m] = App.models[name].instanceMethods[m];
+      }
+
+      for (var m in App.models[name].instanceMethods) {
+        Schema.methods[m] = App.models[name].instanceMethods[m];
+      }
 
       if (App.models[name].mongoosePremodel) {
         App.models[name].mongoosePremodel(Schema);
